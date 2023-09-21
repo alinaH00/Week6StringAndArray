dd5e20a3a0ccbcd6f58ec9903ec2f282697e8b58
Alina Hanselle <alina-hanselle@web.de>
2023-09-21 19:02:30 +0300

    weel6

---
+++ Arrays/Program.cs
@@ -0,0 +1,20 @@
+﻿string[] fruitBasket = { "apples", "bananas", "orange" };
+
+Console.WriteLine($"There are {fruitBasket.Length} fruit in your basket");
+
+//Console.WriteLine(fruitBasket[0]);
+//Console.WriteLine(fruitBasket[1]);
+//Console.WriteLine(fruitBasket[2]);
+
+fruitBasket[0] = "poine-apple";
+fruitBasket[1] = "peaches";
+
+
+//Console.WriteLine(fruitBasket[0]);
+//Console.WriteLine(fruitBasket[1]);
+//Console.WriteLine(fruitBasket[2]);
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
---
+++ Week6StringsAndArrays/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Enter your first name:");
+
+string username = Console.ReadLine().Trim();
+
+
+
+Console.WriteLine(username.Length);
+Console.WriteLine($"The first Letter in your name is {username[0]}");
+
+//replace
+
+string updatedUsername = username.Replace('a', '4').Replace('i','3');
+Console.WriteLine(updatedUsername);
